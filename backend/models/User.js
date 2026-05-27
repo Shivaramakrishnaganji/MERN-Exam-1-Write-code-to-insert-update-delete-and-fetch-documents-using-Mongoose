33a52c1573e421430efea9e50b41d949db170731
Shivaramakrishnaganji <ganjishivaramakrishna@gmail.com>
2026-05-27 12:04:23 +0530

    updated final version code

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-// This is the schema. It tells Mongoose what our data looks like.
-// Think of it as a blueprint for a "User" in our database.
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true // This means a name MUST be provided
+    required: true 
   },
   email: {
     type: String,
@@ -17,5 +15,5 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// We export the model so we can use it in our server.js file
+
 module.exports = mongoose.model('User', userSchema);
